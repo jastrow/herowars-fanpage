bdcd43f1c277217c259c6e72d48b4113a2e2ab4d
Your Name <you@example.com>
2024-06-09 15:23:34 +0200

    API in Zielverzeichnis kopiert

---
+++ docker/html/api/apiConfig.php
@@ -0,0 +1,23 @@
+<?php
+
+ini_set('session.use_trans_sid', 1);
+ini_set('session.use_cookies', 0);
+ini_set('session.use_only_cookies', 0);
+
+const AUTH_TOKEN_VALID_DAY = 1;
+const AUTH_TOKEN_VALID_MONTH = 0;
+const AUTH_TOKEN_VALID_YEAR = 0;
+const AUTH_TOKEN_VALID_SECOND = 0;
+const AUTH_TOKEN_VALID_MINUTE = 0;
+const AUTH_TOKEN_VALID_HOUR = 0;
+const AUTH_INVALID_ERROR_MSG = 'Unauthorized';
+
+const TEXTPAGE_WHITELIST = [
+    'Tipps',
+    'Abenteuer',
+    'Mitglieder',
+    'Regeln',
+    'Gildenaktivität',    
+];
+
+?>
