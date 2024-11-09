3ed9eec3fede55357f51e8474c5fa4d2305e5873
Your Name <you@example.com>
2024-11-09 09:34:40 +0100

    Google Font lokal, zufalls hintergrund beim start

--- src/app/globalsettings.service.ts
+++ src/app/globalsettings.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
@@ -6,11 +6,14 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class GlobalsettingsService {
 
-  private STANDARD_IMAGE = '/assets/mainpagebg30.jpg';
+  private STANDARD_IMAGE = '/assets/poster/Aidan.png';
   private showTopnav$ = new BehaviorSubject<boolean>(true);
   private backgroundImage$ = new BehaviorSubject<string>(this.STANDARD_IMAGE);
 
-  constructor() { }
+  constructor() {
+    let rand = Math.floor(Math.random() * this.posters.length) + 1;
+    this.backgroundImage = '/assets/poster/' + this.posters[rand];
+  }
 
   get showTopnav(): Observable<boolean> {
     return this.showTopnav$.asObservable();
@@ -27,5 +30,73 @@ export class GlobalsettingsService {
     this.backgroundImage$.next(image);
   }
 
+  private posters = [
+    'Aidan.png',
+    'Alvanor.png',
+    'Amira.png',
+    'Andvari.png',
+    'Angus.png',
+    'Arachne.png',
+    'Araji.png',
+    'Artemis.png',
+    'Astaroth.png',
+    'Astrid-Lucas.png',
+    'Augustus.png',
+    'Aurora.png',
+    'Celeste.png',
+    'Cleaver.png',
+    'Cornelius.png',
+    'Corvus.png',
+    'Dante.png',
+    'Daredevil.png',
+    'Darkstar.png',
+    'Dorian.png',
+    'Eden.png',
+    'Elmir.png',
+    'Faceless.png',
+    'Fafnir.png',
+    'Fox.png',
+    'Galahad.png',
+    'Ginger.png',
+    'Heidi.png',
+    'Helios.png',
+    'Iris.png',
+    'Isaac.png',
+    'Ishmael.png',
+    'Jet.png',
+    'Jhu.png',
+    'Jorgen.png',
+    'Judge.png',
+    'Julius.png',
+    'Kai.png',
+    'Karkh.png',
+    'Kayla.png',
+    'Keira.png',
+    'Krista.png',
+    'Lars.png',
+    'Lian.png',
+    'Lilith.png',
+    'Luther.png',
+    'Markus.png',
+    'Martha.png',
+    'Maya.png',
+    'Mojo.png',
+    'Moloch.png',
+    'Mushy-Shroom.png',
+    'Nebula.png',
+    'Orion.png',
+    'Peppy.png',
+    'Phobos.png',
+    'Polaris.png',
+    'Qing-Mao.png',
+    'Rufus.png',
+    'Satori.png',
+    'Sebastian.png',
+    'Solaris.png',
+    'Thea.png',
+    'Tristan.png',
+    'Yasmine.png',
+    'Ziri.png',
+  ];
 
 }
